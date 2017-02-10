cbb023e4340ca7aa49635568afcdfad2b6a12370
Max Rose <Max Rose>
2017-02-10 13:42:00 +0000

    added php script for form handling

---
+++ registercheck.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+
+$sql = "INSERT INTO names (ID, name, firstname, surname, dob )
+VALUES ('[DEFAULT]', '$_POST[username]', '$_POST[firstname]', '$_POST[surname]','$_POST[dob]')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
